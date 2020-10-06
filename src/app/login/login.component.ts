392858f3471a3d9ce6f4c9425b3dc9e45a942f2c
Lookingsoft <lookingsoft@Lookingsofts-MacBook-Air.local>
2020-10-06 15:55:04 +0530

    george

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,6 +6,7 @@ import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { LOGIN } from '../config/endpoints';
 
+//login
 
 @Component({
   selector: 'app-login',
